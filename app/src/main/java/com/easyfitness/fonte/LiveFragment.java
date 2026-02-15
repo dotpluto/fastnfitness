de491521f2e6313911cc327c45d34d6201008434
dotpluto <dotpluto@proton.me>
2026-02-15 14:36:51 +0100

    Add workout picker to live workout view

--- app/src/main/java/com/easyfitness/fonte/LiveFragment.java
+++ app/src/main/java/com/easyfitness/fonte/LiveFragment.java
@@ -8,26 +8,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.easyfitness.DAO.program.DAOProgram;
+import com.easyfitness.DAO.program.Program;
 import com.easyfitness.R;
 
+import java.util.List;
+
 public class LiveFragment extends Fragment {
-
-    public static LiveFragment newInstance(String name, int id) {
-        LiveFragment f = new LiveFragment();
-
-        // Supply index input as an argument.
-        Bundle args = new Bundle();
-        args.putString("name", name);
-        args.putInt("id", id);
-        f.setArguments(args);
-
-        return f;
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_live, container, false);
+        var layout = inflater.inflate(R.layout.tab_live, container, false);
+        getParentFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.liveWorkoutContainer, LivePickerFragment.class, new Bundle())
+                .commit();
+        return layout;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+     public static class FragmentViewModel extends ViewModel {
+        MutableLiveData<Program> selectedProgram = new MutableLiveData<>();
     }
 }
+
+
---
+++ app/src/main/java/com/easyfitness/fonte/LivePickerFragment.java
@@ -0,0 +1,61 @@
+package com.easyfitness.fonte;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.easyfitness.DAO.program.DAOProgram;
+import com.easyfitness.DAO.program.Program;
+import com.easyfitness.R;
+
+import java.util.ArrayList;
+
+public class LivePickerFragment extends Fragment {
+    LiveFragment.FragmentViewModel parentViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        parentViewModel = new ViewModelProvider(requireParentFragment()).get(LiveFragment.FragmentViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        var view = inflater.inflate(R.layout.tab_live_picker, container, false);
+
+        Spinner spinner = view.findViewById(R.id.liveWorkoutPickerSpinner);
+        var context = getContext();
+        assert context != null;
+        var spinnerAdapter = new ArrayAdapter<>(
+                context, android.R.layout.simple_spinner_item,
+                new DAOProgram(this.getContext()).getAll());
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                parentViewModel.selectedProgram.setValue((Program)adapterView.getItemAtPosition(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        return view;
+    }
+
+
+
+}
